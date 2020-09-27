ea7e1a155d9ec3cdc8708c1d23cd984650e4fa66
codereber <jamesmpyle@gmail.com>
2020-09-27 14:39:58 -0400

    update

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -21,11 +21,86 @@ function demoInfo(id) {
             });
         });
     }
+// create function for graphs
+    function Plots(id) {
+        //Read samples.json
+            d3.json("samples.json").then (sampledata =>{
+                console.log(sampledata)
+                var ids = sampledata.samples[0].otu_ids;
+                console.log(ids)
+                var sampleValues =  sampledata.samples[0].sample_values.slice(0,10).reverse();
+                console.log(sampleValues)
+                var labels =  sampledata.samples[0].otu_labels.slice(0,10);
+                console.log (labels)
+            // get only top 10 otu ids for the plot OTU and reversing it. 
+                var OTU_top = ( sampledata.samples[0].otu_ids.slice(0, 10)).reverse();
+            // get the otu id's to the desired form for the plot
+                var OTU_id = OTU_top.map(d => "OTU " + d);
+                console.log(`OTU IDS: ${OTU_id}`)
+             // get the top 10 labels for the plot
+                var labels =  sampledata.samples[0].otu_labels.slice(0,10);
+                console.log(`OTU_labels: ${labels}`)
+                var trace = {
+                    x: sampleValues,
+                    y: OTU_id,
+                    text: labels,
+                    marker: {
+                    color: 'blue'},
+                    type:"bar",
+                    orientation: "h",
+                };
+                // create data variable array from object
+                var data = [trace];
+        
+                // create layout variable to set plots layout
+                var layout = {
+                    title: "Top 10 OTU",
+                    yaxis:{
+                        tickmode:"linear",
+                    },
+                    margin: {
+                        l: 100,
+                        r: 100,
+                        t: 100,
+                        b: 30
+                    }
+                };
+        
+                // create the bar plot
+            Plotly.newPlot("bar", data, layout);
+                // set up the bubble chart
+                var trace1 = {
+                    x: sampledata.samples[0].otu_ids,
+                    y: sampledata.samples[0].sample_values,
+                    mode: "markers",
+                    marker: {
+                        size: sampledata.samples[0].sample_values,
+                        color: sampledata.samples[0].otu_ids
+                    },
+                    text:  sampledata.samples[0].otu_labels
+        
+                };
+        
+                // set the layout for the bubble plot
+                var layout_2 = {
+                    xaxis:{title: "OTU ID"},
+                    height: 600,
+                    width: 1000
+                };
+        
+                // creating data variable 
+                var data1 = [trace1];
+        
+            // create the bubble plot
+            Plotly.newPlot("bubble", data1, layout_2); 
+            
+          });
+        }  
 
 // function for optionChanged event in html to call the other functions
 
 function optionChanged(id) {
-    //  getPlots(id);
+      Plots(id);
       demoInfo(id);
   }
 
@@ -45,7 +120,7 @@ function init() {
         });
 
         // call the functions to display the data and the plots to the page
-     //   getPlots(data.names[0]);
+        Plots(data.names[0]);
         demoInfo(data.names[0]);
     });
 }
