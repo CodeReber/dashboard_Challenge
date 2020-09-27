2dfc50231a33462d15b73700dfcfec7b23646243
codereber <jamesmpyle@gmail.com>
2020-09-27 14:06:12 -0400

    update

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,52 +1,53 @@
-//get data func
-function getdemo_info(id){
-    // d3 to reach jason data
-    d3.json("samples.json").then((data) => {
-        console.log(data);
-       // get meta for demo panel
-        var metadata = data.metadata;
-        // filter it by id
-        var result = metadata.filter(meta => meta.id.toString() === id)[0]
-        // select panel to put data in
-        var demoInfo = d3.select("#sample-metadata");
-
-        // empyt the data in info panel
-        demoInfo.html("");
-        // get the data and from id and append it into the info panel
-        Object.entries(result).forEach((key) => {
-            demoInfo.append("h5").text(key[0].toUpperCase() + ": " + key[1] + "\n");
-
+//function to pull the data with by id
+function demoInfo(id) {
+    // read in the jason data
+        d3.json("samples.json").then((data)=> {
+    // collent the metadata for the demographic panel in the html file
+            var metadata = data.metadata;
+    
+            console.log(metadata)
+    
+          // metadata fileterd info by id
+           var result = metadata.filter(meta => meta.id.toString() === id)[0];
+          // inject the data into the demo panel in the html
+           var demographicInfo = d3.select("#sample-metadata");
+            
+         // empty the dmeo panel each time
+           demographicInfo.html("");
+    
+         // inject demographic data for the id and append the info to the panel html
+            Object.entries(result).forEach((i) => {   
+                demographicInfo.append("h5").text(i[0].toUpperCase() + ": " + i[1] + "\n");    
+            });
         });
-
-    });
-}
+    }
 
 // function for optionChanged event in html to call the other functions
 
-function optionChanged(id){
-    getdemo_info(id);
-}
+function optionChanged(id) {
+    //  getPlots(id);
+      demoInfo(id);
+  }
 
 // create initalize funtion to render everything together
 
-function init(){
-    // select the dropdown menu in html
-    var dropdown = d3.select("#selData");
+function init() {
+    // select dropdown menu 
+    var dropdown = d3.select("#selDataset");
 
-    // read in data
+    // read the data 
     d3.json("samples.json").then((data)=> {
         console.log(data)
 
-        //inject the id to  the dropdown
+        // get the id data to the dropdwown menu
         data.names.forEach(function(name) {
-           dropdown.append("option").text(name).property("value");
+            dropdown.append("option").text(name).property("value");
         });
-        //call the function to display the data
-        getdemo_info(data.names[0]);
 
-       
+        // call the functions to display the data and the plots to the page
+     //   getPlots(data.names[0]);
+        demoInfo(data.names[0]);
     });
-
 }
 
 // call the init to start program
